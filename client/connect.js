3aa6a00633af5746da1a2bb2d3fdb5f419c41cb8
yrttyr <yrttyr@yandex.ru>
2011-11-22 21:49:35 +0600

    в switch_parse теперь используется switch

--- client/connect.js
+++ client/connect.js
@@ -32,63 +32,73 @@ function create_connect() {
 }
 
 function switch_parse(data) {
-    if(data[0] == 'allcoord') {
-        data[1].forEach(function(value) {
-            var indef = value[0];
-            var cells = value[1];
-            for(var key in cells) {
-                var coord = cells[key][0];
-                var info = cells[key][1] || '';
-                game.addObjectInMap(indef, coord, info);
-            }
-        })
-    }
-    else if(data[0] == 'drawdata') {
-        data[1].forEach(function(value) {
-            game.updateDrawdata(value);
-        })
+    switch(data[0]) {
+        case 'allcoord':
+            data[1].forEach(function(value) {
+                var indef = value[0];
+                var cells = value[1];
+                for(var key in cells) {
+                    var coord = cells[key][0];
+                    var info = cells[key][1] || '';
+                    game.addObjectInMap(indef, coord, info);
+                }
+            })
+            break
 
-    }
-    else if(data[0] == 'coord') {
-        data[1].forEach(function(value) {
-            var indef = value[0];
-            var coord = value[1];
-            var info = value[2] || '';
-            game.addObjectInMap(indef, coord, info);
-        })
-    }
-    else if(data[0] == 'gamelist') {
-        data[1].forEach(function(value) {
-            var indef = value[0];
-            var sizeX = value[1];
-            var sizeY = value[2];
-            gamelist.add(indef, sizeX, sizeY);
-        })
-    }
-    else if(data[0] == 'scores') {
-        var n = data[1];
-        var indef = n[0];
-        var color = game.objects[indef].color;
-        var value = n[1];
-        scores.add(color, value);
-    }
-    else if(data[0] == 'gameinfo') {
-        window.game = new Game(data[1][2]);
-        game.setSize(data[1][0], data[1][1]);
-        if(data[1][3]) {
-            game.initMapEditor();
-        }
-        else {
-            game.disableMapEditor();
-        }
-    }
-    else if(data[0] == 'mapslist') {
-        var el = document.getElementById('mapslist');
-        for(var key in data[1]) {
-            var opt = document.createElement('option');
-            opt.innerHTML = data[1][key];
-            opt.value = key;
-            el.appendChild(opt);
-        }
+        case 'drawdata':
+            data[1].forEach(function(value) {
+                game.updateDrawdata(value);
+            })
+            break
+
+        case 'coord':
+            data[1].forEach(function(value) {
+                var indef = value[0];
+                var coord = value[1];
+                var info = value[2] || '';
+                game.addObjectInMap(indef, coord, info);
+            })
+            break
+
+        case 'gamelist':
+            data[1].forEach(function(value) {
+                var indef = value[0];
+                var sizeX = value[1];
+                var sizeY = value[2];
+                gamelist.add(indef, sizeX, sizeY);
+            })
+            break
+
+        case 'scores':
+            var n = data[1];
+            var indef = n[0];
+            var color = game.objects[indef].color;
+            var value = n[1];
+            scores.add(color, value);
+            break
+
+        case 'gameinfo':
+            window.game = new Game(data[1][2]);
+            game.setSize(data[1][0], data[1][1]);
+            if(data[1][3]) {
+                game.initMapEditor();
+            }
+            else {
+                game.disableMapEditor();
+            }
+            break
+
+        case 'mapslist':
+            var el = document.getElementById('mapslist');
+            for(var key in data[1]) {
+                var opt = document.createElement('option');
+                opt.innerHTML = data[1][key];
+                opt.value = key;
+                el.appendChild(opt);
+            }
+            break
+
+        default:
+            console.error('неизвестная команда');
     }
 }
