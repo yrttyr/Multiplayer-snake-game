8d3af9dab2876c0ce4ae1b8638b135b1c58260e5
yrttyr <yrttyr@yandex.ru>
2012-02-05 13:13:29 +0600

    переписал game.js на coffee

--- client/game.js
+++ client/game.js
@@ -1,240 +1,326 @@
-"use strict";
+var Game, Gamemap, GamemapContainer, GamesList, MapsList, Player, PlayersList, needDraw_fill, update,
+  __indexOf = Array.prototype.indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
-var needDraw_fill = function(X, Y) {
-    for (var x = 0; x <= X - 1; x++) {
-        for (var y = 0; y <= Y - 1; y++) {
-            gamemap_cont.needDraw.push([x, y]);
-        }
-    }
+needDraw_fill = function(X, Y) {
+  var x, y, _results;
+  _results = [];
+  for (x = 0; 0 <= X ? x <= X : x >= X; 0 <= X ? x++ : x--) {
+    _results.push((function() {
+      var _results2;
+      _results2 = [];
+      for (y = 0; 0 <= Y ? y <= Y : y >= Y; 0 <= Y ? y++ : y--) {
+        _results2.push(gamemap_cont.needDraw.push([x, y]));
+      }
+      return _results2;
+    })());
+  }
+  return _results;
 };
 
-function GamemapContainer(layers) {
+GamemapContainer = (function() {
+
+  function GamemapContainer(layers) {
+    var layer, name;
     this.canvas = document.getElementById('canvas');
     this.ctx = this.canvas.getContext('2d');
     this.needDraw = [];
-
     window.gamemap = {};
-    for(var name in layers) {
-        gamemap[name] = new Gamemap(layers[name]);
+    for (name in layers) {
+      layer = layers[name];
+      gamemap[name] = new Gamemap(layer);
     }
+  }
 
-    this.setSize = function(x, y) {
-        this.SizeX = x;
-        this.SizeY = y;
-        this.refreshCanvas();
-    };
+  GamemapContainer.prototype.setSize = function(SizeX, SizeY) {
+    this.SizeX = SizeX;
+    this.SizeY = SizeY;
+    return this.refreshCanvas();
+  };
 
-    this.setSizeX = function(x) {
-        this.SizeX = x || parseInt(document.getElementById('Size_X').value);
-        this.refreshCanvas();
-        needDraw_fill(gamemap_cont.SizeX, gamemap_cont.SizeY);
-    };
+  GamemapContainer.prototype.setSizeX = function(x) {
+    this.SizeX = x || parseInt(document.getElementById('Size_X').value);
+    this.refreshCanvas();
+    return needDraw_fill(gamemap_cont.SizeX, gamemap_cont.SizeY);
+  };
 
-    this.setSizeY = function(y) {
-        this.SizeY = y || parseInt(document.getElementById('Size_Y').value);
-        this.refreshCanvas();
-        needDraw_fill(gamemap_cont.SizeX, gamemap_cont.SizeY);
-    };
+  GamemapContainer.prototype.setSizeY = function(y) {
+    this.SizeY = y || parseInt(document.getElementById('Size_Y').value);
+    this.refreshCanvas();
+    return needDraw_fill(gamemap_cont.SizeX, gamemap_cont.SizeY);
+  };
 
-    this.refreshCanvas = function(x, y) {
-        document.getElementById('Size_X').value = this.SizeX;
-        document.getElementById('Size_Y').value = this.SizeY;
-        this.canvas.width = this.SizeX * CELLSIZE;
-        this.canvas.height = this.SizeY * CELLSIZE;
-        this.canvas.style.display = 'block';
-    };
+  GamemapContainer.prototype.refreshCanvas = function() {
+    document.getElementById('Size_X').value = this.SizeX;
+    document.getElementById('Size_Y').value = this.SizeY;
+    this.canvas.width = this.SizeX * CELLSIZE;
+    this.canvas.height = this.SizeY * CELLSIZE;
+    return this.canvas.style.display = 'block';
+  };
 
-    this.addObject = function(indef, coord, info) {
-        var gamemap_name = game.objects[indef].gamemap;
-        gamemap[gamemap_name].set(coord, indef, info);
-    };
-}
+  GamemapContainer.prototype.addObject = function(indef, coord, info) {
+    var gamemap_name;
+    gamemap_name = game.objects[indef].gamemap;
+    return gamemap[gamemap_name].set(coord, indef, info);
+  };
 
-function Gamemap(default_obj_id) {
-    this.dict = {};
+  return GamemapContainer;
+
+})();
+
+Gamemap = (function() {
+
+  function Gamemap(default_obj_id) {
     this.default_obj_id = default_obj_id;
-    this.set = function(k, x, type) {
-        this.dict[k] = {'indef': x, 'type': type || ''};
-        gamemap_cont.needDraw.push(k);
-    },
-    this.setType = function(k, x) {
-        if(k in this.dict) {
-            this.dict[k].type = x;
-            gamemap_cont.needDraw.push(k);
-        }
-        else {
-            console.error('Cell empty', k);
-        }
-    };
-    this.get = function(k) {
-        return this.dict[k] || {'indef': this.default_obj_id, 'type': ''};
-    };
-    this.getType = function(k) {
-        if(k in this.dict) {
-            return this.dict[k].type
-        }
-        return ''
-    };
-    this.getListIdAndCoord = function() {
-        var data = [];
-        for(var key in this.dict) {
-            var indef = this.dict[key].indef;
-            if(indef == 0 || indef == 1) {
-                continue;
-            }
-            var coord = key.split(',');
-            coord = [parseInt(coord[0]), parseInt(coord[1])];
-            if(coord[0] < 0 || coord[0] >= game.SizeX ||
-               coord[1] < 0 || coord[1] >= game.SizeY) {
-                    continue;
-            }
-            data.push([indef, coord]);
-        }
-        return data
-    };
-};
+    this.dict = {};
+  }
 
-function GamesList() {
+  Gamemap.prototype.set = function(k, x, type) {
+    this.dict[k] = {
+      'indef': x,
+      'type': type || ''
+    };
+    return gamemap_cont.needDraw.push(k);
+  };
+
+  Gamemap.prototype.setType = function(k, x) {
+    if (__indexOf.call(this.dict, k) >= 0) {
+      this.dict[k].type = x;
+      return gamemap_cont.needDraw.push(k);
+    } else {
+      return console.error('Cell empty', k);
+    }
+  };
+
+  Gamemap.prototype.get = function(k) {
+    return this.dict[k] || {
+      'indef': this.default_obj_id,
+      'type': ''
+    };
+  };
+
+  Gamemap.prototype.getType = function(k) {
+    if (__indexOf.call(this.dict, k) >= 0) return this.dict[k].type;
+    return '';
+  };
+
+  Gamemap.prototype.getListIdAndCoord = function() {
+    var coord, data, indef, key, value, _ref;
+    data = [];
+    _ref = this.dict;
+    for (key in _ref) {
+      value = _ref[key];
+      indef = value.indef;
+      if (indef === 0 || indef === 1) continue;
+      coord = key.split(',');
+      coord = [parseInt(coord[0]), parseInt(coord[1])];
+      if (coord[0] < 0 || coord[0] >= game.SizeX || coord[1] < 0 || coord[1] >= game.SizeY) {
+        continue;
+      }
+      data.push([indef, coord]);
+    }
+    return data;
+  };
+
+  return Gamemap;
+
+})();
+
+GamesList = (function() {
+
+  function GamesList() {
+    var button, title,
+      _this = this;
     this.div = document.getElementById('games');
     this.maplist = document.getElementById('mapslist');
-
-    var button = document.createElement('input');
+    button = document.createElement('input');
     button.type = 'button';
     button.value = 'Создать игру';
     button.onclick = function() {
-        if(this.maplist.value != '') {
-            connect.sendData([this, 'create_game', this.maplist.value]);
-        }
-    }.bind(this);
+      if (_this.maplist.value !== '') {
+        return connect.sendData([_this, 'create_game', _this.maplist.value]);
+      }
+    };
     this.div.appendChild(button);
-
-    var button = document.createElement('input');
+    button = document.createElement('input');
     button.type = 'button';
     button.value = 'Редактор карт';
     button.onclick = function() {
-        if(this.input.value != '') {
-            connect.sendData([this, 'create_map', this.input.value]);
-            this.input.value = '';
-        }
-    }.bind(this);
+      if (_this.input.value !== '') {
+        connect.sendData([_this, 'create_map', _this.input.value]);
+        return _this.input.value = '';
+      }
+    };
     this.div.appendChild(button);
-
-    var title = document.createElement('div');
+    title = document.createElement('div');
     title.innerHTML = 'Список игр';
     this.div.appendChild(title);
+  }
 
-    this.set = function(indef, data) {
-        var el = document.getElementById(indef);
-        if(!el) {
-            var el = document.createElement('div');
-            el.setAttribute('id', indef);
-            this.div.appendChild(el);
-        }
-        var button = document.createElement('input');
-        button.type = 'button';
-        button.value = indef;
-        button.onclick = function() {
-            connect.sendData([this, 'subscribe_to', indef]);
-        }.bind(this);
-        el.appendChild(button);
+  GamesList.prototype.set = function(indef, data) {
+    var button, el,
+      _this = this;
+    el = document.getElementById(indef);
+    if (!el) {
+      el = document.createElement('div');
+      el.setAttribute('id', indef);
+      this.div.appendChild(el);
     }
-};
-function MapsList() {
+    button = document.createElement('input');
+    button.type = 'button';
+    button.value = indef;
+    button.onclick = function() {
+      return connect.sendData([_this, 'subscribe_to', indef]);
+    };
+    return el.appendChild(button);
+  };
+
+  return GamesList;
+
+})();
+
+MapsList = (function() {
+
+  function MapsList() {
     this.div = document.getElementById('mapslist');
-    this.addList = function(list) {
-        for(var el in list) {
-            this.add(list[el]);
-        }
-    }
-    this.add = function(mapname) {
-        var len = this.div.options.length;
-        this.div.options[len] = new Option(mapname, mapname);
-    }
-}
-function PlayersList() {
-    this.div = document.getElementById('scoreslist');
-    this.set = function(indef, data) {
-        var el = document.getElementById(indef);
-        if(!el) {
-            var el = document.createElement('div');
-            el.setAttribute('id', indef);
-            this.div.appendChild(el);
-        }
-        el.innerHTML = data;
-    }
-}
+  }
 
-function Game() {
+  MapsList.prototype.addList = function(list) {
+    var el, _i, _len, _results;
+    _results = [];
+    for (_i = 0, _len = list.length; _i < _len; _i++) {
+      el = list[_i];
+      _results.push(this.add(el));
+    }
+    return _results;
+  };
+
+  MapsList.prototype.add = function(mapname) {
+    var len;
+    len = this.div.options.length;
+    return this.div.options[len] = new Option(mapname, mapname);
+  };
+
+  return MapsList;
+
+})();
+
+PlayersList = (function() {
+
+  function PlayersList() {
+    this.div = document.getElementById('scoreslist');
+  }
+
+  PlayersList.prototype.set = function(indef, data) {
+    var el;
+    el = document.getElementById(indef);
+    if (!el) {
+      el = document.createElement('div');
+      el.setAttribute('id', indef);
+      this.div.appendChild(el);
+    }
+    return el.innerHTML = data;
+  };
+
+  return PlayersList;
+
+})();
+
+Game = (function() {
+
+  function Game() {
     window.game = this;
     this.canvas = document.getElementById('canvas');
     this.ctx = this.canvas.getContext('2d');
     this.objects = {};
-
     document.getElementById('etitorTools').style.display = 'none';
     document.getElementById('games').style.display = 'block';
+  }
 
-    this.drawAll = function() {
-        for(var key in gamemap_cont.needDraw) {
-            var coord = gamemap_cont.needDraw[key];
-            var ground = gamemap['ground'].get(coord);
-            var base = gamemap['base'].get(coord);
+  Game.prototype.drawAll = function() {
+    var base, base_obj, coord, ground, ground_obj, key, _ref, _results;
+    _ref = gamemap_cont.needDraw;
+    _results = [];
+    for (key in _ref) {
+      coord = _ref[key];
+      ground = gamemap['ground'].get(coord);
+      base = gamemap['base'].get(coord);
+      ground_obj = this.objects[ground.indef];
+      ground_obj.draw(coord[0], coord[1], ground.type);
+      base_obj = this.objects[base.indef];
+      base_obj.draw(coord[0], coord[1], base.type);
+      _results.push(gamemap_cont.needDraw.splice(key, 1));
+    }
+    return _results;
+  };
 
-            var ground_obj = this.objects[ground.indef];
-            ground_obj.draw(coord[0], coord[1], ground.type);
+  Game.prototype.setMapdata = function(x, y, layer) {
+    window.gamemap_cont = new GamemapContainer(layer);
+    gamemap_cont.setSize(x, y);
+    return needDraw_fill(x, y);
+  };
 
-            var base_obj = this.objects[base.indef];
-            base_obj.draw(coord[0], coord[1], base.type);
+  Game.prototype.setListDrawdata = function(list) {
+    var el, _i, _len, _results;
+    _results = [];
+    for (_i = 0, _len = list.length; _i < _len; _i++) {
+      el = list[_i];
+      _results.push(this.setDrawdata(el));
+    }
+    return _results;
+  };
 
-            gamemap_cont.needDraw.splice(key, 1);
-        }
-    };
-    this.setMapdata = function(x, y, layer) {
-        window.gamemap_cont = new GamemapContainer(layer);
-        gamemap_cont.setSize(x, y);
-        needDraw_fill(x, y);
-    };
-    this.setListDrawdata = function(list) {
-        for(var el in list) {
-            this.setDrawdata(list[el]);
-        }
-    };
-    this.setDrawdata = function(data) {
-        this.objects[data['indef']] = new objectTypes[data['drawtype']](data);
-        this.objects[data['indef']].gamemap = data['map_layer']
-    };
-    this.setAllListCoord = function(list) {
-        list.forEach(function(value) {
-            var indef = value[0];
-            var cells = value[1];
-            for(var key in cells) {
-                var coord = cells[key][0];
-                var info = cells[key][1] || '';
-                gamemap_cont.addObject(indef, coord, info);
-            }
-        }, this)
-        requestAnimationFrame(update, game.canvas);
-    };
-    this.setListCoord = function(list) {
-        list.forEach(function(value) {
-            gamemap_cont.addObject(value[0], value[1], value[2]);
-        })
-    };
-}
+  Game.prototype.setDrawdata = function(data) {
+    this.objects[data['indef']] = new objectTypes[data['drawtype']](data);
+    return this.objects[data['indef']].gamemap = data['map_layer'];
+  };
 
-function update() {
-    game.drawAll();
-    requestAnimationFrame(update, game.canvas);
-}
+  Game.prototype.setAllListCoord = function(list) {
+    list.forEach(function(value) {
+      var cell, cells, coord, indef, info, _i, _len, _results;
+      indef = value[0], cells = value[1];
+      _results = [];
+      for (_i = 0, _len = cells.length; _i < _len; _i++) {
+        cell = cells[_i];
+        coord = cell[0];
+        info = cell[1] || '';
+        _results.push(gamemap_cont.addObject(indef, coord, info));
+      }
+      return _results;
+    }, this);
+    return requestAnimationFrame(update, game.canvas);
+  };
 
-function Player() {
-    // ToDo: Удалять события вместе с объектом
+  Game.prototype.setListCoord = function(list) {
+    return list.forEach(function(value) {
+      return gamemap_cont.addObject(value[0], value[1], value[2]);
+    });
+  };
+
+  return Game;
+
+})();
+
+update = function() {
+  game.drawAll();
+  return requestAnimationFrame(update, game.canvas);
+};
+
+Player = (function() {
+
+  function Player() {
+    var _this = this;
     document.onkeydown = function(e) {
-        var e = window.event || e;
-        connect.sendData([this, 'set_rotate', e.keyCode]);
-    }.bind(this);
+      e = window.event || e;
+      return connect.sendData([_this, 'set_rotate', e.keyCode]);
+    };
     document.getElementById('canvas').onmousedown = function(e) {
-        var x = parseInt(e.pageX / CELLSIZE);
-        var y = parseInt(e.pageY / CELLSIZE);
-        connect.sendData([this, 'set_start_coord', x, y]);
-    }.bind(this);
-}
+      var x, y;
+      x = parseInt(e.pageX / CELLSIZE);
+      y = parseInt(e.pageY / CELLSIZE);
+      return connect.sendData([_this, 'set_start_coord', x, y]);
+    };
+  }
+
+  return Player;
+
+})();
---
+++ coffeescript/game.coffee
@@ -0,0 +1,206 @@
+needDraw_fill = (X, Y) ->
+    for x in [0..X]
+        for y in [0..Y]
+            gamemap_cont.needDraw.push([x, y])
+
+class GamemapContainer
+    constructor: (layers) ->
+        @canvas = document.getElementById('canvas')
+        @ctx = @canvas.getContext('2d')
+        @needDraw = []
+
+        window.gamemap = {}
+        for name, layer of layers
+            gamemap[name] = new Gamemap(layer)
+
+    setSize: (@SizeX, @SizeY) ->
+        @refreshCanvas()
+
+    setSizeX: (x) ->
+        @SizeX = x || parseInt(document.getElementById('Size_X').value)
+        @refreshCanvas()
+        needDraw_fill(gamemap_cont.SizeX, gamemap_cont.SizeY)
+
+    setSizeY: (y) ->
+        @SizeY = y || parseInt(document.getElementById('Size_Y').value)
+        @refreshCanvas()
+        needDraw_fill(gamemap_cont.SizeX, gamemap_cont.SizeY)
+
+    refreshCanvas: ->
+        document.getElementById('Size_X').value = @SizeX
+        document.getElementById('Size_Y').value = @SizeY
+        @canvas.width = @SizeX * CELLSIZE
+        @canvas.height = @SizeY * CELLSIZE
+        @canvas.style.display = 'block'
+
+    addObject: (indef, coord, info) ->
+        gamemap_name = game.objects[indef].gamemap
+        gamemap[gamemap_name].set(coord, indef, info)
+
+
+class Gamemap
+    constructor: (@default_obj_id) ->
+        @dict = {}
+    set: (k, x, type) ->
+        @dict[k] = {'indef': x, 'type': type || ''}
+        gamemap_cont.needDraw.push(k)
+
+    setType: (k, x) ->
+        if k in @dict
+            @dict[k].type = x
+            gamemap_cont.needDraw.push(k)
+        else
+            console.error('Cell empty', k)
+
+    get: (k) ->
+        return @dict[k] || {'indef': @default_obj_id, 'type': ''}
+
+    getType: (k) ->
+        if k in @dict
+            return @dict[k].type
+        return ''
+
+    getListIdAndCoord: ->
+        data = []
+        for key, value of @dict
+            indef = value.indef
+            if indef == 0 or indef == 1
+                continue
+            coord = key.split(',')
+            coord = [parseInt(coord[0]), parseInt(coord[1])];
+            if(coord[0] < 0 or coord[0] >= game.SizeX or
+               coord[1] < 0 or coord[1] >= game.SizeY)
+                    continue
+            data.push([indef, coord])
+        return data
+
+class GamesList
+    constructor: ->
+        @div = document.getElementById('games')
+        @maplist = document.getElementById('mapslist')
+
+        button = document.createElement('input')
+        button.type = 'button'
+        button.value = 'Создать игру'
+        button.onclick = =>
+            if @maplist.value != ''
+                connect.sendData([@, 'create_game', @maplist.value])
+        @div.appendChild(button)
+
+        button = document.createElement('input')
+        button.type = 'button'
+        button.value = 'Редактор карт'
+        button.onclick = =>
+            if @input.value != ''
+                connect.sendData([@, 'create_map', @input.value])
+                @input.value = ''
+        @div.appendChild(button);
+
+        title = document.createElement('div')
+        title.innerHTML = 'Список игр'
+        @div.appendChild(title)
+
+    set: (indef, data) ->
+        el = document.getElementById(indef)
+        if not el
+            el = document.createElement('div')
+            el.setAttribute('id', indef)
+            @div.appendChild(el)
+
+        button = document.createElement('input')
+        button.type = 'button'
+        button.value = indef
+        button.onclick = =>
+            connect.sendData([@, 'subscribe_to', indef]);
+        el.appendChild(button)
+
+class MapsList
+    constructor: ->
+        @div = document.getElementById('mapslist');
+
+    addList: (list) ->
+        for el in list
+            this.add el
+
+    add: (mapname) ->
+        len = @div.options.length
+        @div.options[len] = new Option(mapname, mapname)
+
+class PlayersList
+    constructor: ->
+        @div = document.getElementById('scoreslist')
+
+    set: (indef, data) ->
+        el = document.getElementById(indef)
+        if not el
+            el = document.createElement('div')
+            el.setAttribute('id', indef)
+            @div.appendChild(el)
+        el.innerHTML = data
+
+class Game
+    constructor: ->
+        window.game = @
+        @canvas = document.getElementById('canvas')
+        @ctx = @canvas.getContext('2d')
+        @objects = {}
+
+        document.getElementById('etitorTools').style.display = 'none'
+        document.getElementById('games').style.display = 'block'
+
+    drawAll: ->
+        for key, coord of gamemap_cont.needDraw
+            ground = gamemap['ground'].get(coord)
+            base = gamemap['base'].get(coord)
+
+            ground_obj = @objects[ground.indef]
+            ground_obj.draw(coord[0], coord[1], ground.type)
+
+            base_obj = @objects[base.indef]
+            base_obj.draw(coord[0], coord[1], base.type)
+
+            gamemap_cont.needDraw.splice(key, 1)
+
+    setMapdata: (x, y, layer) ->
+        window.gamemap_cont = new GamemapContainer(layer)
+        gamemap_cont.setSize(x, y)
+        needDraw_fill(x, y)
+
+    setListDrawdata: (list) ->
+        for el in list
+            @setDrawdata(el)
+
+    setDrawdata: (data) ->
+        @objects[data['indef']] = new objectTypes[data['drawtype']](data)
+        @objects[data['indef']].gamemap = data['map_layer']
+
+    setAllListCoord: (list) ->
+        list.forEach((value) ->
+            [indef, cells] = value
+            for cell in cells
+                coord = cell[0]
+                info = cell[1] || ''
+                gamemap_cont.addObject(indef, coord, info)
+        , @)
+        requestAnimationFrame(update, game.canvas);
+
+    setListCoord: (list) ->
+        list.forEach((value) ->
+            gamemap_cont.addObject(value[0], value[1], value[2])
+        )
+
+update = ->
+    game.drawAll()
+    requestAnimationFrame(update, game.canvas)
+
+class Player
+    # ToDo: Удалять события вместе с объектом
+    constructor: ->
+        document.onkeydown = (e) =>
+            e = window.event || e
+            connect.sendData([@, 'set_rotate', e.keyCode])
+
+        document.getElementById('canvas').onmousedown = (e) =>
+            x = parseInt(e.pageX / CELLSIZE)
+            y = parseInt(e.pageY / CELLSIZE)
+            connect.sendData([@, 'set_start_coord', x, y])
